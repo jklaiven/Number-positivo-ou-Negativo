39e786b6e8090478260df69985823d268b2ee6c5
jklaiven <87581758+jklaiven@users.noreply.github.com>
2024-09-09 17:04:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um número: "))
+
+if n1 > 0:
+  print(f"O número {n1} é positivo!")
+elif n1 < 0:
+  print(f"O número {n1} é negativo!")
+else:
+  print(f"O número {n1} é zero.")
